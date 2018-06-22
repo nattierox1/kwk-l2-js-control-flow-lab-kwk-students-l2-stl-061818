6ccb663a9116e495edb2702ed49491d50f6047ae
nattierox1 <nattierox1@gmail.com>
2018-06-22 14:18:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ function scuberGreetingForFeet(input) {
   else if (input>2000) {
     return ('I will gladly take your thirty bucks.')
   }
-  else if (input<=2500)
+  else if (input<=2500) {
+    return ('No can do.')
+  }
+  
 }
 
 
