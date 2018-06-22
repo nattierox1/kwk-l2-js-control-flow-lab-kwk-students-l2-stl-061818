9151df1a12299e68374a2dc2e5a2db09a58355e3
nattierox1 <nattierox1@gmail.com>
2018-06-22 14:21:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // Write your code in this file!
 function scuberGreetingForFeet(input) {
   if (input<=400) {
-    return ('This one is on me!')
+    return ('This one is on me!');
   }
   else if (input>2000 && input <= 2500) {
-    return ('I will gladly take your thirty bucks.')
+    return ('I will gladly take your thirty bucks.');
   }
   else if (input=>2500) {
-    return ('No can do.')
+    return ('No can do.');
   }
   
 }
 
-function ternaryCheckCity()
+function ternaryCheckCity(place)
+
