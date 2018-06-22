aecf14b1bd1c12a0d8af6cf19bcf1161827a094f
nattierox1 <nattierox1@gmail.com>
2018-06-22 14:14:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+function scuberGreetingForFeet(input) {
+  if (input<=400) {
+    return ('This one is on me!')
+  }
+  else {}
+}
+
+ternaryCheckCity();
+switchCharmCity();
+switchOnCharmFromTip();
+return 
