acb71c9de9923bf8310aa67151fbb55b2947beda
nattierox1 <nattierox1@gmail.com>
2018-06-22 14:39:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet(input) {
+  if (input<=400) {
+    return 'This one is on me!';
+  }
+  else if (input>2000 && input <= 2500) {
+    return ('I will gladly take your thirty bucks.');
+  }
+  else if (input=>2500) {
+    return ('No can do.');
+  }
+}
+  function ternaryCheckCity(place) {
+    return (place ==='NYC' ? "Ok, sounds good." : 'No go.');
+  }
+  
+  function switchOnCharmFromTip(response) {
+    switch (response) {
+      case "generous" :
+        return "Thank you so much." ;
+      case "not as generous" :
+        return "Thank you." ;
+      default:
+        return "Bye."
+    }
+  }
+
+
+
+
